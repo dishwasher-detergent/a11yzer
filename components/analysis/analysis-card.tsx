9b04724a72fa867a7a5634dcaf2d8ea82ae98bb1
Kenneth Bass <kenny_bass@outlook.com>
2025-08-31 13:18:03 -0500

    add appwrite stuff and update card

--- components/analysis/analysis-card.tsx
+++ components/analysis/analysis-card.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { LucideDot } from "lucide-react";
 import Link from "next/link";
 
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { AnalysisDb, AnalysisResult } from "@/interfaces/analysis.interface";
 
 export function AnalysisCard(analysis: AnalysisDb<AnalysisResult>) {
@@ -19,21 +12,26 @@ export function AnalysisCard(analysis: AnalysisDb<AnalysisResult>) {
     <Card className="break-inside-avoid-column rounded-lg overflow-hidden py-0 gap-0 ">
       <CardContent className="p-0 relative">
         <AspectRatio ratio={1} className="w-full">
-          {analysis.data.screenshotUrl ? (
-            <img
-              src={analysis.data.screenshotUrl}
-              alt={analysis.data.analysis.summary.slice(0, 25)}
-              className="object-cover object-left-top bg-primary"
-            />
-          ) : (
-            <div className="w-full aspect-square bg-muted grid place-items-center">
-              <p className="text-muted-foreground font-semibold">
-                No Screenshot
-              </p>
-            </div>
-          )}
+          <img
+            src={analysis.data.screenshotUrl}
+            alt={analysis.data.analysis.summary.slice(0, 25)}
+            className="object-cover object-left-top bg-primary"
+          />
         </AspectRatio>
-        <CardHeader className="flex flex-col justify-end bottom-0 absolute w-full p-4 h-full bg-linear-to-t from-primary to-primary/20">
+        <p className="z-10 flex flex-row gap-2 text-xs items-center absolute top-2 left-2 w-full">
+          <span className="max-w-1/2 truncate">{analysis.team?.name}</span>/
+          <span className="max-w-1/2 truncate">{analysis.user?.name}</span>
+        </p>
+        <CardHeader className="flex flex-col justify-end bottom-0 absolute w-full p-4 h-full bg-linear-to-t from-background to-background/20">
+          <p className="flex flex-row text-muted-foreground text-xs items-center">
+            {new Date(analysis.$createdAt).toLocaleString(undefined, {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </p>
           <CardTitle className="text-primary-foreground">
             <Button
               className="p-0! text-primary-foreground text-base text-wrap whitespace-normal h-auto"
@@ -47,13 +45,8 @@ export function AnalysisCard(analysis: AnalysisDb<AnalysisResult>) {
               </Link>
             </Button>
           </CardTitle>
-          <CardDescription className="text-primary-foreground text-xs">
-            {new Date(analysis.$createdAt).toLocaleDateString("en-US")}
-          </CardDescription>
-          <p className="flex flex-row text-primary-foreground text-xs items-center">
-            <span>{analysis.team?.name}</span>
-            <LucideDot />
-            <span>{analysis.user?.name}</span>
+          <p className="flex flex-row text-muted-foreground text-xs items-center">
+            {analysis.data.url}
           </p>
         </CardHeader>
       </CardContent>
