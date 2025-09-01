7b9b096ebec82325bd8c71b589d23390c8705c7d
Kenneth Bass <kenny_bass@outlook.com>
2025-09-01 00:41:08 -0500

    test

--- lib/analysis/browser.ts
+++ lib/analysis/browser.ts
@@ -4,6 +4,9 @@ import puppeteerCore from "puppeteer-core";
 
 export const dynamic = "force-dynamic";
 
+const remoteExecutablePath =
+  "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar";
+
 export async function getBrowser() {
   if (process.env.NODE_ENV === "development") {
     const browser = await puppeteer.launch({
@@ -23,7 +26,7 @@ export async function getBrowser() {
         "--single-process",
         "--disable-gpu",
       ],
-      executablePath: await chromium.executablePath(),
+      executablePath: await chromium.executablePath(remoteExecutablePath),
     });
 
     return browser;
