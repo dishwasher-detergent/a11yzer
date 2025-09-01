660b98a2279943ca7115a3d21e654586591faf9e
Kenneth Bass <kenny_bass@outlook.com>
2025-09-01 01:40:24 -0500

    test

--- lib/analysis/browser.ts
+++ lib/analysis/browser.ts
@@ -1,6 +1,8 @@
 import chromium from "@sparticuz/chromium";
+import { existsSync } from "fs";
 import { readdir } from "fs/promises";
 import { tmpdir } from "os";
+import { join } from "path";
 import puppeteer from "puppeteer";
 import puppeteerCore from "puppeteer-core";
 
@@ -15,15 +17,22 @@ export async function getBrowser() {
 
     return browser;
   } else {
-    try {
-      const files = await readdir(tmpdir());
-      console.log("Files in /tmp:", files);
-    } catch (error) {
-      console.error("Error reading /tmp directory:", error);
-    }
     const path = await chromium.executablePath(
       "/usr/local/server/src/function/node_modules/@sparticuz/chromium/bin"
     );
+
+    try {
+      const files = await readdir(tmpdir());
+      console.log("Files in /tmp:", files);
+
+      const exists = existsSync(join(tmpdir(), "chromium"));
+      console.log("Chromium exists in /tmp:", exists);
+
+      const nextFiles = await readdir("/usr/local/server/src/function/.next");
+      console.log("Files in .next:", nextFiles);
+    } catch (error) {
+      console.error("Error reading /tmp directory:", error);
+    }
 
     console.log("Chromium executable path:", path);
     const browser = await puppeteerCore.launch({
