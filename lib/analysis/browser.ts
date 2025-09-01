f9e097c47f301652930bbd51841c5adccf127498
Kenneth Bass <kenny_bass@outlook.com>
2025-09-01 01:05:24 -0500

    test

--- lib/analysis/browser.ts
+++ lib/analysis/browser.ts
@@ -1,4 +1,7 @@
 import chromium from "@sparticuz/chromium";
+import { readdir } from "fs/promises";
+import { tmpdir } from "os";
+import puppeteer from "puppeteer";
 import puppeteerCore from "puppeteer-core";
 
 export const dynamic = "force-dynamic";
@@ -8,21 +11,20 @@ const remoteExecutablePath =
 
 export async function getBrowser() {
   if (process.env.NODE_ENV === "development") {
-    const browser = await puppeteerCore.launch({
+    const browser = await puppeteer.launch({
       headless: true,
-      args: [
-        ...chromium.args,
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--single-process",
-        "--disable-gpu",
-      ],
-      executablePath: await chromium.executablePath(remoteExecutablePath),
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
     return browser;
   } else {
+    try {
+      const files = await readdir(tmpdir());
+      console.log("Files in /tmp:", files);
+    } catch (error) {
+      console.error("Error reading /tmp directory:", error);
+    }
+
     const browser = await puppeteerCore.launch({
       headless: true,
       args: [
