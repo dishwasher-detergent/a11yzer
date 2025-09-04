cf16664906c79b9afa1fb6ba690ccdb44cdff419
Kenneth Bass <kenny_bass@outlook.com>
2025-09-03 19:03:13 -0500

    fix ui for add member

--- components/team/actions.tsx
+++ components/team/actions.tsx
@@ -22,7 +22,7 @@ export function TeamActions({
       <h3 className="font-semibold text-base mb-2">Actions</h3>
       <div className="flex flex-row gap-1">
         {!isOwner && <LeaveTeam team={data} />}
-        {isMember && <InviteTeam team={data} />}
+        {(isAdmin || isOwner) && <InviteTeam team={data} />}
         {(isOwner || isAdmin) && <EditTeam team={data} />}
         {isOwner && <DeleteTeam team={data} />}
       </div>
