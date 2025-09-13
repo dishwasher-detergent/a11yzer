af476c4e40e5a3c131e4a56ad08da4934f685ba6
Kenneth Bass <kenny_bass@outlook.com>
2025-09-13 09:53:36 -0500

    remove hue

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,3 @@
-import { Hue } from "@/components/hue";
-
 export default async function Layout({
   children,
 }: Readonly<{
@@ -8,9 +6,6 @@ export default async function Layout({
   return (
     <main className="grid min-h-dvh p-2 md:p-0 w-full md:place-items-center bg-muted relative">
       {children}
-      <div className="absolute top-1/2 translate-y-1/2 w-full hidden md:grid place-items-center z-0">
-        <Hue />
-      </div>
     </main>
   );
 }
